8627dec4b3d5dc74cc15bf3759183905f0afa4f8
shweta-bambal <shwetabambal18@gmail.com>
2021-07-25 17:04:14 +0530

    InitialModel

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,33 @@
+﻿using API.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            return await _context.AppUsers.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async  Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            return await _context.AppUsers.FindAsync(id);
+        }
+    }
+}
---
+++ API/Models/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Models
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options) { }
+        public DbSet<AppUser> AppUsers { get; set; }
+
+    }
+}
